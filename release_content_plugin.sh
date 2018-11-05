8c48e0f2ea1d56173a6d2b54b771643e9719b8ce
tux <tux@sql.example.com>
2018-11-05 12:06:17 +0000

    create plug in

--- release_content_plugin.sh
+++ release_content_plugin.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo cp -r ./public_html/plugins/content/$1 /var/www/html/velopoint.co.uk/public_html/plugins/content/$1
+
+if [ $# -eq 0 ]
+	then echo "Enter the name of the plugin"
+else
+	sudo cp -r "./public_html/plugins/content/$1/" "/var/www/html/velopoint.co.uk/public_html/plugins/content/"
+fi
