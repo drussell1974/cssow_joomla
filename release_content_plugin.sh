25019e49e5ee3e2931d61e43bc850f6c510c3ba7
tux <tux@server1.example.com>
2018-11-13 08:30:31 +0000

    create basic plugin

--- release_content_plugin.sh
+++ release_content_plugin.sh
@@ -3,5 +3,23 @@
 if [ $# -eq 0 ]
 	then echo "Enter the name of the plugin"
 else
-	sudo cp -r "./public_html/plugins/content/$1/" "/opt/lampp/htdocs/webalizer/tmp/"
+	echo "1. Creating zip in dev... com_$1.zip"
+        sudo zip -r "src/com_$1.zip" "src/$1"
+	
+
+	echo "2. Copy component files to site plugins directory..."
+	sudo mkdir "/opt/lampp/htdocs/webalizer/plugins/content/$1"
+	sudo cp -r "./src/$1/" "/opt/lampp/htdocs/webalizer/plugins/content"
+	sudo chown -R daemon:daemon "/opt/lampp/htdocs/webalizer/plugins/content/$1"
+        sudo ls -l "/opt/lampp/htdocs/webalizer/htdocs/webalizer/plugins/content/$1"
+
+
+	#echo "2. Zip component to site tmp"
+	
+	#sudo zip -r "/opt/lampp/htdocs/webalizer/tmp/com_$1.zip" "src/$1"
+	#sudo chown -R daemon:daemon "/opt/lampp/htdocs/webalizer/tmp/$1"
+	#echo "Check permissions..."
+	#sudo ls -l "/opt/lampp/htdocs/webalizer/tmp/$1"
+	
+	echo "Complete!"
 fi
