f99a3599fa3293f5a9a2b151e375140bfc43e173
daveanson <dave@jazzthecat.co.uk>
2018-11-16 05:12:39 +0000

    Joomla site with Basic MVC Component

---
+++ src/site/administrator/components/com_schemeofwork/admin/schemeofwork.php
@@ -0,0 +1,2 @@
+<?php 
+    echo "SOW administration";
---
+++ src/site/administrator/components/com_schemeofwork/site/views/schemeofwork/view.html.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * HTML View class for the SchemeOfWork Component
+ *
+ * @since  0.0.1
+ */
+class schemeofworkViewschemeofwork extends JViewLegacy
+{
+	/**
+	 * Display the schemeofwork view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+            // Assign data to the view
+            $this->msg = 'Scheme of Work';
+
+            // Display the view
+            parent::display($tpl);
+	}
+}
