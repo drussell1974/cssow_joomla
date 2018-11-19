29eab51403d7d90a4a028417bf010bea26cd81c8
daveanson <dave@jazzthecat.co.uk>
2018-11-19 16:34:17 +0000

    Add Custom Debug Logging

--- src/site/administrator/components/com_schemeofwork/site/schemeofwork.php
+++ src/site/administrator/components/com_schemeofwork/site/schemeofwork.php
@@ -9,7 +9,20 @@
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-        
+
+JLog::addLogger(
+        array(
+             // Sets file name
+             'text_file' => JText::_('LOG_FILENAME')
+        ),
+            // Sets messages of all log levels to be sent to the file
+        JLog::ALL,
+            // The log category/categories which should be recorded in this file
+            // In this case, it's just the one category from our extension, still
+            // we need to put it inside an array
+        array(JText::_('LOG_CATEGORY'))
+    );      
+
 // Get an instance of the controller prefixed by SchemeOfWork
 $controller = JControllerLegacy::getInstance('schemeofwork');
 
