0a3302ea5750003b9356f6b1f0ea61b7f7b65355
daveanson <dave@jazzthecat.co.uk>
2018-12-02 16:30:38 +0000

    Add delete content function

--- src/site/administrator/components/com_schemeofwork/admin/controllers/contents.php
+++ src/site/administrator/components/com_schemeofwork/admin/controllers/contents.php
@@ -34,4 +34,16 @@ class SchemeOfWorkControllerContents extends JControllerAdmin {
         return $model;
     }
 
+    public function delete(){
+        $input = JFactory::getApplication()->input;
+        $recs = $input->get('cid', array(), 'array');
+        $nrecs = $input->get('boxchecked', 0 , 'int');
+        $model = $this->getModel('Content', 'SchemeOfWorkModel');
+        $model->delete($recs);
+        $msg = JText::sprintf('COM_SCHEMEOFWORK_CONTENT_N_ITEMS_DELETED' ,$nrecs);
+        //... then redirect to the same page
+        
+        $this->setRedirect(JRoute::_('index.php?option=com_schemeofwork&view=contents', false), $msg);
+    }
+
 }
